68a7d51649b8aa268cb2599aead2c1b13cc119b0
Beth Day <beth.ellieday@gmail.com>
2021-08-15 16:09:18 -0400

    updated php code for contact page.

--- mailto_handler.php
+++ mailto_handler.php
@@ -1,6 +1,18 @@
 <?php
-    if($_POST['message']){
-       mail("bethmarsh1018@gmail.com", "Message",
-       $_POST["Message from"]. "From: $email");
+    if(isset($_POST['submit'])){
+        $name=$_POST['name'];
+        $email=$_POST['email'];
+        $msg=$_POST['msg'];
+
+        $to='bethmarsh1018@gmail.com';
+        $subject='Form Submission';
+        $message="Name: ".$name."\n". "Wrote the following: "."\n\n".$msg;
+        $headers="From: ".$email;
+
+        if(mail($to, $subject, $message, $headers)){
+            echo "<h1>Message Sent! Thank you"." ".$name.".</h1>";
+        } else {
+            echo "Something went wrong.";
+        }
     }
 ?>
